f98216eedf4bfe0f941dc0dc07403470541e0ab4
stylian <stchatzim@gmail.com>
2016-09-22 13:18:40 +0100

    proper path and gitignore

--- src/restclient/Main.java
+++ src/restclient/Main.java
@@ -13,7 +13,6 @@ public class Main {
 	public static final String GET = "GET";
 	public static final String POST = "POST";
 	public static final String PUT = "PUT";
-	public static final String PATCH = "PATCH";
 	public static final String DELETE = "DELETE";
 	
 	public static void main(String[] args) {

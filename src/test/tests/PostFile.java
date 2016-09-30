0d92eadb2e3c15823ebb0831c2e0674978a44315
stylian <stchatzim@gmail.com>
2016-09-30 10:54:10 +0100

    added post file test basis

---
+++ src/test/tests/PostFile.java
@@ -0,0 +1,31 @@
+package test.tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import main.restclient.Method;
+import main.restclient.ServiceManager;
+
+public class PostFile {
+
+	private ServiceManager serv;
+	private Writer writer;
+	
+	@Before
+	public void init() {
+		writer = new StringWriter();
+		serv = new ServiceManager(writer);	
+	}
+	
+	@Test
+	public void postFile() throws IOException {
+		//TODO
+	}
+}

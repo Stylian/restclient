30b2f33cc454de9c56bfc2fb9108c61f607ede13
stylian <stchatzim@gmail.com>
2016-09-30 10:45:38 +0100

    added testing output file for json

--- src/test/tests/RestTest.java
+++ src/test/tests/RestTest.java
@@ -27,24 +27,24 @@ public class RestTest {
 	@Test
 	public void get() throws IOException {
 		serv.send("http://jsonplaceholder.typicode.com/posts/1", Method.GET);
-		Assert.assertEquals("get failed" , FileUtils.readFileToString(new File("src/test/resources/get.txt")), serv.JsonOutput);
+		Assert.assertEquals("get failed" , FileUtils.readLines(new File("src/test/resources/json_responses.txt")).get(0), serv.JsonOutput);
 	}
 
 	@Test
 	public void post() throws IOException {
 		serv.send("http://jsonplaceholder.typicode.com/posts", Method.POST, "{ \"id\": \"1\", \"title\": \"foo\", \"body\": \"bar\", \"userId\": \"1\" }");
-		System.out.println(writer);
+		Assert.assertEquals("get failed" , FileUtils.readLines(new File("src/test/resources/json_responses.txt")).get(1), serv.JsonOutput);
 	}
 	
 	@Test
 	public void put() throws IOException {
 		serv.send("http://jsonplaceholder.typicode.com/posts/1", Method.PUT, "{ \"id\": \"1\", \"title\": \"foo2\", \"body\": \"bar\", \"userId\": \"1\" }");
-		System.out.println(writer);
+		Assert.assertEquals("get failed" , FileUtils.readLines(new File("src/test/resources/json_responses.txt")).get(2), serv.JsonOutput);
 	}
 	
 	@Test
 	public void delete() throws IOException {
 		serv.send("http://jsonplaceholder.typicode.com/posts/1", Method.DELETE);
-		System.out.println(writer);
+		Assert.assertEquals("get failed" , FileUtils.readLines(new File("src/test/resources/json_responses.txt")).get(3), serv.JsonOutput);
 	}
 }

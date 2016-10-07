e54ada72567b5dbf739950bbff06d33de20f6927
stylian <stchatzim@gmail.com>
2016-10-07 12:16:47 +0100

    test commit from egit

--- src/test/tests/PostFile.java
+++ src/test/tests/PostFile.java
@@ -1,16 +1,11 @@
 package test.tests;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.io.Writer;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import main.restclient.Method;
 import main.restclient.ServiceManager;
 
 public class PostFile {
--- src/test/tests/RestTest.java
+++ src/test/tests/RestTest.java
@@ -8,7 +8,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import main.restclient.Method;
 import main.restclient.ServiceManager;
 
 public class RestTest {

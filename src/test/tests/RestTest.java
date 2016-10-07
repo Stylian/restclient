ef6aedf80adf408fc6762e40f9d82168b6baea37
stylian <stchatzim@gmail.com>
2016-10-07 15:48:35 +0100

    fixed msg

--- src/test/tests/RestTest.java
+++ src/test/tests/RestTest.java
@@ -29,21 +29,21 @@ public class RestTest {
 	@Test
 	public void post() throws IOException {
 		serv.post("http://jsonplaceholder.typicode.com/posts", "{ \"id\": \"1\", \"title\": \"foo\", \"body\": \"bar\", \"userId\": \"1\" }");
-		Assert.assertEquals("get failed" , FileUtils.readLines(new File("src/test/resources/json_responses.txt")).get(1), serv.getMetadata().getResponse());
+		Assert.assertEquals("post failed" , FileUtils.readLines(new File("src/test/resources/json_responses.txt")).get(1), serv.getMetadata().getResponse());
 		Assert.assertEquals(201, serv.getMetadata().getStatusCode());
 	}
 	
 	@Test
 	public void put() throws IOException {
 		serv.put("http://jsonplaceholder.typicode.com/posts/1", "{ \"id\": \"1\", \"title\": \"foo2\", \"body\": \"bar\", \"userId\": \"1\" }");
-		Assert.assertEquals("get failed" , FileUtils.readLines(new File("src/test/resources/json_responses.txt")).get(2), serv.getMetadata().getResponse());
+		Assert.assertEquals("put failed" , FileUtils.readLines(new File("src/test/resources/json_responses.txt")).get(2), serv.getMetadata().getResponse());
 		Assert.assertEquals(200, serv.getMetadata().getStatusCode());
 	}
 	
 	@Test
 	public void delete() throws IOException {
 		serv.delete("http://jsonplaceholder.typicode.com/posts/1", "");
-		Assert.assertEquals("get failed" , FileUtils.readLines(new File("src/test/resources/json_responses.txt")).get(3), serv.getMetadata().getResponse());
+		Assert.assertEquals("delete failed" , FileUtils.readLines(new File("src/test/resources/json_responses.txt")).get(3), serv.getMetadata().getResponse());
 		Assert.assertEquals(200, serv.getMetadata().getStatusCode());
 	}
 }

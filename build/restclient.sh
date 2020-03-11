16af8a15f2f28382ad0c5e1a540891ba8a74b0dd
Stylian <stylianos.chatzimanolis@dotmatics.com>
2020-03-11 16:37:17 +0200

    made body to work

--- build/restclient.sh
+++ build/restclient.sh
@@ -4,4 +4,14 @@ my_dir="$(dirname "$0")"
 
 source $my_dir/config.ini
 
-"$java_path" --enable-preview -jar $my_dir/console.jar $1 $2
+method="$1"
+url="$2"
+shift
+shift
+body="$@"
+
+echo method: $method
+echo url: $url
+echo body: $body
+echo -----------------
+"$java_path" --enable-preview -jar $my_dir/console.jar "$method" "$url" "$body"

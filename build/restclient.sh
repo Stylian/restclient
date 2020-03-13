d16f3682b83fdb099dba6a36dff967af2d68bbc1
Stylian <stylianos.chatzimanolis@dotmatics.com>
2020-03-13 16:00:57 +0200

    added dynamic params

--- build/restclient.sh
+++ build/restclient.sh
@@ -12,6 +12,20 @@ body="$@"
 
 echo method: $method
 echo url: $url
+
+var=($(grep -F __param__ $my_dir/config.ini | grep -Fv "#"))
+added_params="$(IFS=; printf '%s' "${var[*]/__param__/&}")"
+echo additional parameters: $added_params
+
 echo body: $body
 echo -----------------
+
+if [[ $url == *"?"* ]]; then
+  url=${url}${added_params}
+else
+  url=${url}?r=1${added_params}
+fi
+
+echo final_url: $url
+
 "$java_path" --enable-preview -jar $my_dir/console.jar "$method" "$url" "$body"
